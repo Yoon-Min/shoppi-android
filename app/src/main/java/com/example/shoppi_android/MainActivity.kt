54aa31d8b0800e9ad6dd738569e30ae689ee55c4
Yoon-Min <yoonmin0113@naver.com>
2022-07-07 02:11:06 +0900

    [섹션6] 쇼핑앱 UI 구성 : 하단 탭 구성

--- app/src/main/java/com/example/shoppi_android/MainActivity.kt
+++ app/src/main/java/com/example/shoppi_android/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.shoppi_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
+
+//        find, get은 찾으려는 값이 없을 수 있으므로 ?. 연산자를 꼭 사용하자
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 
 
